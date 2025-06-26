add150cddf119b313b86eeefde77bfeffe0aaa4f
Fabricio-Alvarez <jerry.alvarez@ucr.ac.cr>
2025-06-25 20:36:40 -0600

    Fabricio siendo persona 2 y estoy creando los métodos para actualizar y listar, ademas de añadir el endpoint correspondiente

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MaterialController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/materiales', [MaterialController::class, 'index'])->name('materials.index');
+Route::get('/material/edit', [MaterialController::class, 'edit'])->name('materials.edit');
+Route::put('/material', [MaterialController::class, 'update'])->name('materials.update');
+
 
 Route::get('/material/create', [MaterialController::class, 'create'])->name('materials.create');
