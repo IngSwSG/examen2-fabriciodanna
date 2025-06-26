4070eecbbd0de2d25a5d5bf1f0ed726a1a2727b3
dannaJarquin <danna.jarquin@ucr.ac.cr>
2025-06-25 20:17:09 -0600

    Danna siendo persona 1 y estoy creando el controller que se usará y sus metodos para crear, ademas de añadir el endpoint correspondiente

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/material/create', [MaterialController::class, 'create'])->name('materials.create');
